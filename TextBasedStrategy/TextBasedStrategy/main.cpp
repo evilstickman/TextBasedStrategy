f093ea7fad88b2ea742e0259b34941b8704b0ca5
mbillock <mbillock@enova.com>
2013-07-31 16:04:20 -0500

    Simple hello world code. This will need to change structure to be  actually worthwhile

---
+++ TextBasedStrategy/TextBasedStrategy/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  TextBasedStrategy
+//
+//  Created by mbillock on 7/31/13.
+//
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
