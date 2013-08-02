54a12796baa68b2adb000a005a73b4035c02a573
mbillock <mbillock@enova.com>
2013-08-02 13:55:27 -0500

    Updating notes and comments

--- TextBasedStrategy/TextBasedStrategy/main.cpp
+++ TextBasedStrategy/TextBasedStrategy/main.cpp
@@ -12,7 +12,7 @@
 int main(int argc, const char * argv[])
 {
 
-    // insert code here...
+    // This is a cheap method that only works for linux/osx. Windows will need to create text art some other way
     std::cout << ANSI_CLEAR_SCREEN<< ANSI_MOVE_TO_1_1 << ANSI_ESCAPE_YELLOW << "Hello, World!" << ANSI_ESCAPE_RESET << "\n";
     return 0;
 }
