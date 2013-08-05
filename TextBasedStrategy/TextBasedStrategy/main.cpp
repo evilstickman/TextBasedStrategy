d79e18d655445774ffd2c3e98a703316ca0a7c9d
mbillock <mbillock@enova.com>
2013-08-05 17:08:53 -0500

    Drawing dots to screen

--- TextBasedStrategy/TextBasedStrategy/main.cpp
+++ TextBasedStrategy/TextBasedStrategy/main.cpp
@@ -14,6 +14,15 @@ int main(int argc, const char * argv[])
 
     // This is a cheap method that only works for linux/osx. Windows will need to create text art some other way
     std::cout << ANSI_CLEAR_SCREEN<< ANSI_MOVE_TO_1_1 << ANSI_ESCAPE_YELLOW << "Hello, World!" << ANSI_ESCAPE_RESET << "\n";
+    std::cout<<ANSI_MOVE_TO_1_1;
+    for(int i = 1; i <= 40; ++i)
+    {
+        for(int j=1; j <= 160; ++j)
+        {
+            std::cout << "\033["<<i<<";"<<j<<"H"<< '.';
+        }
+    }
+    std::cout << std::endl;
     return 0;
 }
 
